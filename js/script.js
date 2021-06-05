abaf0388be554262936caaa7e3a6d0a532989a99
ianmcdavid <ianmcdavid25@gmail.com>
2021-06-04 17:55:51 -0700

    Populated repo list using GitHub API and JS

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@
 const overview = document.querySelector(".overview");
 //My username
 const username = "ianmcdavid";
-//Get profile info from GitHub
+//Repo list UL
+const repoList = document.querySelector(".repo-list");
+//Get profile info from GitHub API
 const getUser = async function() {
     const userResults = await fetch(`https://api.github.com/users/${username}`);
     const userData = await userResults.json();
     displayUser(userData);
-    console.log(userData);
+    //console.log(userData);
     
 };
 getUser();
+
 
 //Display Profile Info
 const displayUser = function(userData) {
@@ -26,4 +29,25 @@ const displayUser = function(userData) {
     <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
   </div>`;
     overview.append(infoDiv);
+};
+
+//Get repo data from GitHub API
+const getRepos = async function() {
+    const repoResults = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoResults.json();
+    
+    //console.log(listItem);
+    displayRepos(repoData);
+};
+getRepos();
+
+//Displays Repo Info
+const displayRepos = function(repoData) {
+    for (let repo of repoData) {
+        let listItem = repo;
+        listItem = document.createElement("li");
+        listItem.classList.add("repo");
+        listItem.innerHTML = `<h3>${repo.name}`;
+        repoList.append(listItem);
+    }
 };
